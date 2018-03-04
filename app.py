74a9719cb78862ffa750a43f0e3c7c149babf9af
Max Xu <xujialv@gmail.com>
2018-03-03 19:54:36 -0500

    fix bug & update requirements.txt

--- app.py
+++ app.py
@@ -3,39 +3,43 @@ from werkzeug.utils import redirect
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-  return render_template("index.html")
+    return render_template("index.html")
+
 
 @app.route("/about")
 def about():
-  return render_template("about.html")
+    return render_template("about.html")
+
 
 @app.route('/concat', methods=['GET', 'POST'])
 def concat():
-  import StringIO
-  import numpy as np
-  from PIL import Image
-  from skimage.io import imsave
-  if request.method == 'POST':
-    if 'image' not in request.files:
-      imagefile = Image.open('static/img/1.tif')
-    else:
-      imagefile = Image.open(request.files['image'])
-    try:
-      arr = np.asarray(imagefile)
-      for i in range(3):
-        arr = np.concatenate((arr, np.rot90(imagefile, i + 1)), axis=1)
+    import StringIO
+    import numpy as np
+    from PIL import Image
+    from skimage.io import imsave
+    if request.method == 'POST':
+        if 'image' not in request.files:
+            imagefile = Image.open('static/img/1.tif')
+        else:
+            imagefile = Image.open(request.files['image'])
+        try:
+            arr = np.asarray(imagefile)
+            for i in range(3):
+                arr = np.concatenate((arr, np.rot90(imagefile, i + 1)), axis=1)
 
-      strIO = StringIO.StringIO()
-      imsave(strIO, arr, plugin='pil', format_str='png')
-      strIO.seek(0)
-      return send_file(strIO, mimetype='image/png')
-    except Exception as err:
-      if err:
-        flash('File not supported')
-        return render_template('warning.html')
+            strIO = StringIO.StringIO()
+            imsave(strIO, arr, plugin='pil', format_str='png')
+            strIO.seek(0)
+            return send_file(strIO, mimetype='image/png')
+        except Exception as err:
+            if err:
+                flash('File not supported')
+                return render_template('warning.html')
+
 
 if __name__ == "__main__":
-  app.secret_key = 'key'
-  app.run(debug=True)
+    app.secret_key = 'key'
+    app.run(debug=True)
