241244348d80e526e17eb868d8b3cc362dc92aac
Max Xu <xujialv@gmail.com>
2018-03-03 19:26:15 -0500

    set up basic app

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, send_file, flash
+from werkzeug.utils import redirect
 
 app = Flask(__name__)
 
@@ -10,5 +11,31 @@ def index():
 def about():
   return render_template("about.html")
 
+@app.route('/concat', methods=['POST'])
+def concat():
+  import StringIO
+  import numpy as np
+  from PIL import Image
+  from skimage.io import imsave
+
+  if 'image' not in request.files:
+    imagefile = Image.open('static/img/1.tif')
+  else:
+    imagefile = Image.open(request.files['image'])
+  try:
+    arr = np.asarray(imagefile)
+    for i in range(3):
+      arr = np.concatenate((arr, np.rot90(imagefile, i + 1)), axis=1)
+
+    strIO = StringIO.StringIO()
+    imsave(strIO, arr, plugin='pil', format_str='png')
+    strIO.seek(0)
+    return send_file(strIO, mimetype='image/png')
+  except Exception as err:
+    if err:
+      flash('File not supported')
+      return render_template('warning.html')
+
 if __name__ == "__main__":
+  app.secret_key = 'key'
   app.run(debug=True)
