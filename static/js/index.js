a8a30d7991d911180bf541377b6c0c046c6953a6
Max Xu <xujialv@gmail.com>
2018-03-08 20:40:09 -0500

    update styles and form scripts

--- static/js/index.js
+++ static/js/index.js
@@ -1,14 +1,87 @@
+$(document).ready(function() {
+    $('select').material_select();
 
-document.addEventListener("DOMContentLoaded", function() {
 
-    document.getElementById("process").addEventListener("click", function(e) {
-    if( document.getElementById("file").files.length == 0 ){
+    $('select').change(function() {
+        var e = document.getElementById("label_num");
+        var num = e.options[e.selectedIndex].value;
+        createForm(num);
+    });
 
-        e.preventDefault();
-        alert("Please select a file!");
+    $('#form-container').on('click', '#process', function() {
+        $('.preloader-wrapper').removeClass('hidden').addClass('active');
+        $('.progress').removeClass('hidden');
+    });
+
+
+
+function createForm(numOfLabels) {
+
+  if (document.getElementById("form")) {
+    var f = document.getElementById("form");
+    f.parentNode.removeChild(f);
+  }
+
+  if (!document.getElementById("process")) {
+    var f = document.createElement("form");
+    f.setAttribute('method',"post");
+    f.setAttribute('id',"form");
+    f.setAttribute('action',"render");
+    f.setAttribute('enctype',"multipart/form-data");
+
+    var input_container = document.createElement("div");
+        input_container.setAttribute('class',"row");
+
+    for (var i = 0; i < numOfLabels; ++i) {
+
+        var div = document.createElement("div");
+        div.setAttribute('class',"input-field col s12");
+
+        var label = document.createElement("label");
+        label.setAttribute('for',"label" + parseInt(i + 1));
+        label.innerHTML = "Label Name " + parseInt(i + 1);
+        var input = document.createElement("input");
+        input.setAttribute('type',"text");
+        input.setAttribute('name',"label" + parseInt(i + 1));
+        input.setAttribute('id',"label" + parseInt(i + 1));
+        var input_file = document.createElement("input");
+        input_file.setAttribute('type',"file");
+        input_file.setAttribute('multiple',"multiple");
+        input_file.setAttribute('name',"file" + parseInt(i + 1));
+        input_file.setAttribute('accept',"image/*");
+        div.appendChild(label);
+        div.appendChild(input);
+        div.appendChild(input_file);
+        input_container.appendChild(div);
     }
 
-    })
+        var s = document.createElement("input"); //input element, Submit button
+        s.setAttribute('type',"submit");
+        s.setAttribute('class',"btn waves-effect waves-light");
+        s.setAttribute('id',"process");
+        s.setAttribute('value',"Process");
+        f.appendChild(input_container);
+        f.appendChild(s);
 
 
-});
+
+        document.getElementById('form-container').appendChild(f);
+    }
+
+
+}
+
+
+ });
+
+
+//    document.getElementById("process").addEventListener("click", function(e) {
+//    if( document.getElementById("file").files.length == 0 ){
+//
+//        e.preventDefault();
+//        alert("Please select files!");
+//    }
+
+
+
+
