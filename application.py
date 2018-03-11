88e9e987b24b27f78cef788a220cde00b46039fe
Max Xu <xujialv@gmail.com>
2018-03-11 14:30:37 -0400

    add gpu

--- application.py
+++ application.py
@@ -18,16 +18,7 @@ import pandas
 from sklearn.manifold import TSNE
 # from threading import Thread
 # from time import sleep
-import tensorflow as tf
 
-
-with tf.device('/gpu:0'):
-    a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
-    b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
-    c = tf.matmul(a, b)
-
-with tf.Session() as sess:
-    print (sess.run(c))
 
 
 application = Flask(__name__)
