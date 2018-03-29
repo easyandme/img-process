00b10d653dac6675c19fc9048b75493040bca81f
Max Xu <xujialv@gmail.com>
2018-03-29 13:20:06 -0400

    open host

--- application.py
+++ application.py
@@ -201,4 +201,4 @@ def ae_encoder():
 if __name__ == "__main__":
     application.secret_key = 'key'
     port = int(os.environ.get("PORT", 5000))
-    application.run(port=port, debug=False)
+    application.run(host='0.0.0.0', port=port, debug=False)
